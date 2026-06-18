bdd0f4bf959c0374f53e7345159e28904a0756e7
Vitthal Dwivedi <chitranshiii13@gmail.com>
2026-06-19 02:17:25 +0530

    Initial commit: Setup C++ basic repository

---
+++ C++/basics.cpp
@@ -0,0 +1,193 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x;
+    cin>>x;
+    cout<<"Hello"<< x+2;
+
+
+    // string s1 , s2;
+    // cin >> s1>>s2;
+    // cout<<s1<<" "<<s2;
+
+    // string var;
+    // getline(cin , var);
+    // cout<<var;
+
+    // char ch = 'A'; //Character decalaration
+    // cin>> ch;
+    // cout<<ch;
+
+    // int age;
+    // cin >> age;
+    // if (age >=18)
+    // {
+    //     cout << " i am an adult";
+    // }
+
+    // else
+    // {
+    //     cout << "i am a child";
+    // }
+
+    // else if(age<15)
+    // {
+    //     cout << "i am a child";
+    // }
+
+    // int marks;
+    // cin>>marks;
+    // if(marks<25){
+    //  cout<<"F";
+    // }
+    // if(marks>=25 && marks<45){
+    //     cout<<"E";
+    // }
+    // if(marks>=45 && marks<50){
+    //     cout<<"D";
+    // }
+    // if(marks>=50 && marks<60){
+    //     cout<<"C";
+    // }
+    // if(marks>=60 && marks<80){
+    //     cout<<"B";
+    // }
+    // if(marks>=80 && marks<=100){
+    //     cout<<"A";
+    // }
+
+    //       if(marks<25){
+    //      cout<<"F";
+    //     }
+    //     else if(marks<45){
+    //         cout<<"E";
+    //     }
+    //     else if(marks<50){
+    //         cout<<"D";
+    //     }
+    //     else if(marks<60){
+    //         cout<<"C";
+    //     }
+    //     else if( marks<80){
+    //         cout<<"B";
+    //     }
+    //     else if(marks<=100){
+    //         cout<<"A";
+    //     }
+
+    //     int age;
+    //     cin >> age;
+    //     if (age < 18)
+    //     {
+    //         cout << "not eligible for a job";
+    //     }
+    //     else if (age <= 57)
+    //     {
+    //         cout << "i m eligible for a job";
+    //         if (age >=55)
+    //         {
+    //             cout << " " << "but retirement soon";
+    //         }
+    //         else if(age > 57)
+    //         {
+    //             cout << "retirement time";
+    //         }
+    //         return 0;
+    //     }
+    // }
+
+    // int day;
+    // cin>>day;
+    // switch(day)
+    // {
+    //     case 1:
+    //     cout<<"Monday";
+    //     break;
+    //     case 2:
+    //     cout<<"Tuesday";
+    //     break;
+    //     case 3:
+    //     cout<<"Wednesday";
+    //     break;
+    //     case 4:
+    //     cout<<"Thursday";
+    //     break;
+    //     case 5:
+    //     cout<<"Friday";
+    //     break;
+    //     case 6:
+    //     cout<<"Saturday";
+    //     break;
+    //     case 7:
+    //     cout<<"Sunday";
+    //     break;
+    //     default:
+    //     cout<<"Invalid day";
+    // }
+    // cout<<" "<< "Hello world";
+    // return 0;
+    // }
+
+    // Arrays
+    //  1D array
+
+    // int arr[5];
+
+    // float arr[5];
+    // cin>>arr[0]>>arr[1]>>arr[2]>>arr[3]>>arr[4];
+    // arr[3]+=2;
+
+    // arr[3] = 16; //replacement
+    // cout<<arr[3];
+    // return 0;
+    // }
+
+    // 2D array
+
+    // int arr[2][3];
+    // arr[1][2]=56;
+    // cout<<arr[1][2];
+    // return 0;
+    // }
+
+    // Strings
+
+    // string s = "striver";
+    // int len = s.size(); //can use s.length() also
+    // s[len-1] = 's';
+    // cout<<s[len-1];
+    // return 0;
+    //  }
+
+    // For Loop
+    // int i;
+    // for (i = 1; i<=25; i+=5)
+    // {
+    //     cout << "striver" << " " << i << endl;
+    // }
+    // cout<<i<<endl;
+    //     return 0;
+    // }
+
+    // while loop
+
+    // int i=1;
+    // while(i<=5){
+    //     cout<<"striver"<<" "<<i<<endl;
+    //     i++;
+    // }
+    // return 0;
+    // }
+
+    // do while loop
+//     int i = 2;
+//     do
+//     {
+//         cout << "striver" <<" " << i << endl;
+//         i++;
+//     } while (i < 1);
+//     cout << i;
+//     return 0;
+// }
+
