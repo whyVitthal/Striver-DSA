f3b7139951738e2f2b51ee9fec888361b07275c4
Vitthal Dwivedi <chitranshiii13@gmail.com>
2026-06-24 23:07:25 +0530

    patterns updated

---
+++ patterns.cpp
@@ -0,0 +1,92 @@
+#include <iostream>
+using namespace std;
+
+void print1(int n)
+// yha se online compiler me code krna ho 
+{
+    int i,j;
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < n; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+}
+void print2(int n)
+{
+    int i,j;
+    for (i = 0; i <n; i++)
+    {
+        for (j = 0; j <=i; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
+void print3(int n){
+    int i,j;
+    for (i = 1; i <=n; i++)
+    {
+        for (j = 1; j <=i; j++)
+        {
+            cout <<j<<" ";
+        }
+        cout << endl;
+    }
+}
+void print4(int n){
+    int i,j;
+    for (i = 1; i <=n; i++)
+    {
+        for (j = 1; j <=i; j++)
+        {
+            cout <<i<<" ";
+        }
+        cout << endl;
+    }
+}
+void print5(int n){
+    int i,j;
+    for (i = 0; i <=n; i++)
+    {
+        for(j=n;j>i;j--)
+        {
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+}
+
+// backend of an online compiler 
+int main()
+{  
+    int t;
+    cin>>t;
+    for(int i=0;i<t;i++)
+    {   
+    int n;
+    cin>>n;
+    print5(n);
+    }
+    return 0;
+}
+
+// testcase:
+// Input: 2 
+// 3
+// 5
+// so 2 batata hai ki patterb 2 times print hoga phle 3 phr 5 ke liye patterns print honge 
+
+// output :
+// ***
+// ***
+// ***
+// *****
+// *****
+// *****
+// *****
+// *****
