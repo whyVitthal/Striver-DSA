b5eaee9130b791d55d4d825df708628784f40dd0
Vitthal Dwivedi <chitranshiii13@gmail.com>
2026-07-01 04:12:16 +0530

    patterns completed

--- patterns.cpp
+++ patterns.cpp
@@ -155,8 +155,103 @@ void print11(int n){
         cout<<endl;
     }
 }
+void pattern12(int n){
+    int i,j;
+    for( i=1;i<=n;i++){
+    //numbers
+    for(j=1;j<=i;j++){
+        cout<<j; 
+    }
+    //spaces
+    int spaces = 2*(n-i);
+    for(j=1;j<=spaces;j++){
+        cout<<" "; 
+    }
+    //numbers
+    for(j=i;j>=1;j--){
+        cout<<j; 
+    }
+    cout<<endl;
+    spaces = spaces - 2;
+    }
+}
+void pattern14(int n){
+    int i,j;
+    for(i=0;i<n;i++){
+        for(char ch='A'; ch <='A'+ i;ch++){
+            cout<<ch<<" ";
+        }
+        cout<<endl;
+    }
+}
+void pattern15(int n){
+    int i,j;
+    for(i=0;i<n;i++){
+        for(char ch='A';ch<='A'+(n-i-1);ch++){
+            cout<<ch<<" ";
+        }
+        cout<<endl;
+    }
+}
+void pattern16(int n){
+ int i,j;
+ for(i=0;i<n;i++){
+    char ch='A'+i;
+        for(j=0;j<=i;j++){
+            cout<<ch <<" ";
+        }
+        cout<<endl;
+    }
+}
+void pattern17(int n){
+    int i,j;
+    for(i=0;i<n;i++)
+    {
+       for(j=0;j<n-i-1;j++)
+       {
+           cout<<" "; //space print krne k liye
+       } 
+       char ch ='A';
+       int breakpoint = (2*i+1)/2;
+       for(j=0;j<2*i+1;j++)
+       {
+           cout<<ch<<" "; 
+           if(j<breakpoint){
+               ch++;
+           } else {
+               ch--;
+           }
+       }
+       for(j=0;j<n-i-1;j++)
+       {
+           cout<<" "; //space print krne k liye
+       }
+         cout<<endl;
+    }
+}
+void pattern18(int n){
+    int i,j;
+    for(i=0;i<n;i++){
+        for(char ch='E'-i;ch<='E';ch++){
+            cout<<ch<<" ";
+        }
+        cout<<endl;
+    }
+}
+void pattern21(int n){
+    int i,j;
+ for(i=0;i<n;i++){
+    for(j=0;j<n;j++){
+    if(i==0 || i==n-1 || j==0 || j==n-1){
+        cout<<"*";
+    } else {
+        cout<<" ";
+    }
 
-
+ }  
+ cout<<endl; 
+ }
+}
 
 
 
@@ -172,7 +267,7 @@ int main()
     cin>>n;
     // print8(n);
     // print9(n);
-    print11(n);
+    pattern21(n);
     }
     return 0;
 }
