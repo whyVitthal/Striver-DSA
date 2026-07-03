11f9f29526dc9b1e98b9176274616290fb7c9bbc
Vitthal Dwivedi <chitranshiii13@gmail.com>
2026-07-03 22:56:44 +0530

    STL notes

---
+++ STL.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    // pair<int,int>p={1,2};
+    // cout<<p.first<<" "<<endl<<p.second;
+
+
+    // pair<int,int> arr[]={{1,2},{3,4}};
+    // cout<<arr[1].second;
+
+    
+    return 0;
+}
